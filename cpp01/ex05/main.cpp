284aedcf124dcea26f6ab5cfb9530dca2ef5b9e1
Henri Geffroy <hgeffroy@z4r9p7.42lyon.fr>
2023-10-16 11:36:53 +0200

    lala

--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hgeffroy <hgeffroy@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: hgeffroy <hgeffroy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 08:56:45 by hgeffroy          #+#    #+#             */
-/*   Updated: 2023/10/14 10:15:14 by hgeffroy         ###   ########.fr       */
+/*   Updated: 2023/10/16 11:35:34 by hgeffroy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 int main(void)
 {
-	Harl	test;
+	Harl	harl;
 
-	std::cout << "Level not defined: ";
-	test.complain("UBF");
-	std::cout << "Level DEBUG";
-	test.complain("DEBUG");
-	std::cout << "Level INFO";
-	test.complain("INFO");
-	std::cout << "Level WARNING";
-	test.complain("WARNING");
-	std::cout << "Level ERROR";
-	test.complain("ERROR");
+	std::cout << "Level not defined: " << std::endl;
+	harl.complain("UBF");
+	std::cout << "Level DEBUG: " << std::endl;
+	harl.complain("DEBUG");
+	std::cout << "Level INFO: " << std::endl;
+	harl.complain("INFO");
+	std::cout << "Level WARNING: " << std::endl;
+	harl.complain("WARNING");
+	std::cout << "Level ERROR: " << std::endl;
+	harl.complain("ERROR");
 }
