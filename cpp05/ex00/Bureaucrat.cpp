d2c5f89303663fa7e1c2c08cfbe72d3b2acc843d
Henri Geffroy <hgeffroy@z4r7p3.42lyon.fr>
2023-10-29 13:22:25 +0100

    Lala les exceptions le debut

---
+++ cpp05/ex00/Bureaucrat.cpp
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hgeffroy <hgeffroy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/29 10:25:06 by hgeffroy          #+#    #+#             */
+/*   Updated: 2023/10/29 13:20:09 by hgeffroy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : _name("Undefined"), _grade(1)
+{
+	std::cout << "Bureaucrat default constructor called" << std::endl;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "Bureaucrat default destructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat const& copyFrom) : _name(copyFrom._name), _grade(copyFrom._grade)
+{	
+	std::cout << "Bureaucrat copy constructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(std::string name, int grade) : _name(name)
+{
+	if (grade < 1 || grade > 150)
+		; // Throw l'exception
+	else
+		this->_grade = grade;
+	
+	std::cout << "Bureaucrat parametric constructor called" << std::endl;
+}
+
+Bureaucrat& Bureaucrat::operator=(Bureaucrat const& rightValue)
+{
+	if (this->_name != rightValue._name)
+	{
+		std::cout << "The name wasn't the same, " << this->_name << " unchanged." << std::endl;
+		return (*this);
+	}
+	this->_grade = rightValue._grade;
+
+	return (*this);
+}
+
+std::ostream& operator<<(std::ostream& os, Bureaucrat const& rightValue)
+{
+	os << rightValue._name << ", bureaucrat grade " << rightValue._grade << ".";
+	
+	return (os);
+}
+
+Bureaucrat& Bureaucrat::operator++()
+{
+	if (this->_grade == 1)
+		; // Throw
+	else
+		this->_grade--;
+}
+
+Bureaucrat& Bureaucrat::operator--()
+{
+	if (this->_grade == 150)
+		; // Throw
+	else
+		this->_grade++;
+}
+	
+std::string	Bureaucrat::getName()
+{
+	return (this->_name);
+}
+
+int	Bureaucrat::getGrade()
+{
+	return (this->_grade);
+}
+
---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hgeffroy <hgeffroy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/29 10:19:12 by hgeffroy          #+#    #+#             */
+/*   Updated: 2023/10/29 13:21:12 by hgeffroy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <string>
+# include <stdexcept>
+
+class Bureaucrat
+{
+	
+private:
+
+	std::string const	_name;
+	int					_grade;
+	
+public:
+
+	// Declarer les exceptions ici.
+
+	Bureaucrat();
+	~Bureaucrat();
+	Bureaucrat(Bureaucrat const& copyFrom);
+	Bureaucrat(std::string name, int grade);
+
+	Bureaucrat& operator=(Bureaucrat const& rightValue);
+	Bureaucrat& operator++();
+	Bureaucrat& operator--();
+	friend std::ostream& operator<<(std::ostream& os, Bureaucrat const& rightValue);
+
+	std::string	getName();
+	int			getGrade();
+	
+};
+
+#endif
