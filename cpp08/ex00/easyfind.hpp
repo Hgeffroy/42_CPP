fe267182bb5da674d0745b1a631efd33c0a82902
Henri Geffroy <hgeffroy@z4r7p5.42lyon.fr>
2023-11-10 15:29:03 +0100

    lalala

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hgeffroy <hgeffroy@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/10 13:39:16 by hgeffroy          #+#    #+#             */
+/*   Updated: 2023/11/10 14:31:55 by hgeffroy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+template <typename T>
+int	easyfind(T container, int n)
+{
+
+}
+
+#endif
